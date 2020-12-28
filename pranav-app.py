0e3b5c522b6af6c2faf543f76ad2fe3504145d3d
pranavadityanath <76592017+pranavadityanath@users.noreply.github.com>
2020-12-28 20:54:20 +0530

    Update pranav-app.py

--- pranav-app.py
+++ pranav-app.py
@@ -1,9 +1,9 @@
 # Short code
 import tweepy
-consumer_key = 'dE6zSkvmqSFqLpaQSQQ0DXDdH'
-consumer_secret = 'Fr6vbc4SBujaKUeCBdWuF3uLDkNppRacC1UBD8b29jz9moKCfQ'
-access_token = '1339935823723053058-BrM9rJ4KQrpj5hKKJSE84TacCDL2iG'
-secret_access_token = 'GHkoUlElkImJVScqioHZPin0haukdzr3VxxbKw11qah92'
+api_key = 'dE6zSkvmqSFqLpaQSQQ0DXDdH'
+api_secret_key = 'Fr6vbc4SBujaKUeCBdWuF3uLDkNppRacC1UBD8b29jz9moKCfQ'
+access_token = '1339935823723053058-Fxljvo4mXFWYlF1hQsZv27RvVAV0tg'
+secret_access_token = 'fuJlZCHkEqwYuJRvy9eWpnEswFItY5y9JjDLySHflgjvn'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, secret_access_token)
