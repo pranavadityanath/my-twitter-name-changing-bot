070a05816fdc2e2dfb1f415fd3daaaff8340fb89
pranavadityanath <76592017+pranavadityanath@users.noreply.github.com>
2020-12-28 19:22:35 +0530

    Update ameer-app.py

--- ameer-app.py
+++ ameer-app.py
@@ -1,9 +1,9 @@
 # Short code
 import tweepy
-consumer_key = 'kg5MaWSyuM2yKcJuXmjrq7eCk'
-consumer_secret = 'XHcELNRB1qr2ZXouiBcQWyz95C8RY0WK9tt4Npn5YGEtNqvYcV'
-access_token = '1297164703110582274-HRFFBP4VNHSNss4u0npVcOYwpVBf6J'
-secret_access_token = 'GgibeHs5ntOovfWFJOHuG9e88CnzNp7wSYqVCr6qyuTJO'
+consumer_key = 'dE6zSkvmqSFqLpaQSQQ0DXDdH'
+consumer_secret = 'Fr6vbc4SBujaKUeCBdWuF3uLDkNppRacC1UBD8b29jz9moKCfQ'
+access_token = '1339935823723053058-BrM9rJ4KQrpj5hKKJSE84TacCDL2iG'
+secret_access_token = 'GHkoUlElkImJVScqioHZPin0haukdzr3VxxbKw11qah92'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, secret_access_token)
@@ -14,6 +14,6 @@ import time
 while True:
   user = api.get_user('Ameen91741779')
   f = user.followers_count
-  api.update_profile(name=f'AMEER {f} Followers')
-  print(f'AMEER {f} Followers')
+  api.update_profile(name=f'PRANAV {f} Followers')
+  print(f'PRANAV {f} Followers')
   time.sleep(60)
